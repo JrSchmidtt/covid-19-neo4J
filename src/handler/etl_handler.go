53c29b3093f58eef363ea1355491bac7b0355e39
Junior Schmidt <junior.schmidt.99@gmail.com>
2024-12-21 19:43:00 -0300

    Add support for processing COVID-19 and global COVID-19 data files in ETL handler

--- src/handler/etl_handler.go
+++ src/handler/etl_handler.go
@@ -10,6 +10,7 @@ import (
 )
 
 var comma = ';' // default comma separator
+var commaCovidGlobal = ','
 
 func ProcessCSV(c *gin.Context) {
 	vaccinationDataFile, _, err := c.Request.FormFile("vaccination")
@@ -27,10 +28,11 @@ func ProcessCSV(c *gin.Context) {
 		c.JSON(500, gin.H{"error": "internal server error"})
 		return
 	}
+
 	vaccineDataFile, _, err := c.Request.FormFile("vaccine")
 	if err != nil {
 		log.Println(err)
-		c.JSON(400, gin.H{"error": "bad request please provide a file with key 'vaccine-data'"})
+		c.JSON(400, gin.H{"error": "bad request please provide a file with key 'vaccine'"})
 		return
 	}
 	defer vaccineDataFile.Close()
@@ -42,8 +44,43 @@ func ProcessCSV(c *gin.Context) {
 		c.JSON(500, gin.H{"error": "internal server error"})
 		return
 	}
+
+	covidDataFile, _, err := c.Request.FormFile("covid")
+	if err != nil {
+		log.Println(err)
+		c.JSON(400, gin.H{"error": "bad request please provide a file with key 'covid'"})
+		return
+	}
+	defer covidDataFile.Close()
+	covidReader := csv.NewReader(covidDataFile)
+	covidReader.Comma = comma
+	covidRawData, err := utils.ExtractDataFromCSV(covidReader)
+	if err != nil {
+		log.Println(err)
+		c.JSON(500, gin.H{"error": "internal server error"})
+		return
+	}
+
+	covidGlobalDataFile, _, err := c.Request.FormFile("covid_global")
+	if err != nil {
+		log.Println(err)
+		c.JSON(400, gin.H{"error": "bad request please provide a file with key 'covid_global'"})
+		return
+	}
+	defer covidGlobalDataFile.Close()
+	covidGlobalReader := csv.NewReader(covidGlobalDataFile)
+	covidGlobalReader.Comma = commaCovidGlobal
+	covidGlobalRawData, err := utils.ExtractDataFromCSV(covidGlobalReader)
+	if err != nil {
+		log.Println(err)
+		c.JSON(500, gin.H{"error": "internal server error"})
+		return
+	}
+
 	c.JSON(http.StatusOK, gin.H{
 		"vaccinationRawData": vaccinationRawData,
 		"vaccineRawData":     vaccineRawData,
+		"covidRawData":       covidRawData,
+		"covidGlobalRawData": covidGlobalRawData,
 	})
 }
