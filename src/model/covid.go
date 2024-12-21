9bc6518b7e5c5060ab323e293fb162d628fc8385
Junior Schmidt <junior.schmidt.99@gmail.com>
2024-12-21 19:42:47 -0300

    Rename CovidStats model to Covid and add CovidGlobal model for comprehensive tracking of global Covid-19 data

--- src/model/covid.go
+++ src/model/covid.go
@@ -3,7 +3,7 @@ package model
 import "time"
 
 // Represents a Covid-19 along the time and world regions.
-type CovidStats struct {
+type Covid struct {
 	ID               string    `json:"id"` // Internal ID for the record
 	DateReported     time.Time `json:"date_reported"`
 	CountryCode      string    `json:"country_code"`
@@ -15,4 +15,23 @@ type CovidStats struct {
 	CumulativeDeaths int64     `json:"cumulative_deaths"`
 	CreatedAt        time.Time `json:"created_at"` // Date when the record was created
 	UpdatedAt        time.Time `json:"updated_at"` // Date when the record was updated
+}
+
+// Represents a Covid-19 global data.
+type CovidGlobal struct {
+	ID                      string    `json:"id"` // Internal ID for the record
+	Country                 string    `json:"country"`
+	WHORegion               string    `json:"who_region"`
+	CumulativeCases         int64     `json:"cumulative_cases"`
+	CumulativeCasesPer100k  int64     `json:"cumulative_cases_per100k"`
+	NewCases7Days           int64     `json:"new_cases_7_days"`
+	NewCases7DaysPer100k    int64     `json:"new_cases_7_days_per100k"`
+	NewCases24Hours         int64     `json:"new_cases_24_hours"`
+	CumulativeDeaths        int64     `json:"cumulative_deaths"`
+	CumulativeDeathsPer100k int64     `json:"cumulative_deaths_per100k"`
+	NewDeaths7Days          int64     `json:"new_deaths_7_days"`
+	NewDeaths7DaysPer100k   int64     `json:"new_deaths_7_days_per100k"`
+	NewDeaths24Hours        int64     `json:"new_deaths_24_hours"`
+	CreatedAt               time.Time `json:"created_at"` // Date when the record was created
+	UpdatedAt               time.Time `json:"updated_at"` // Date when the record was updated
 }
