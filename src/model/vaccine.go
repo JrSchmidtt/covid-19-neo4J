d2ac8cb8e4647372b37f7c425225d5022506e447
Junior Schmidt <junior.schmidt.99@gmail.com>
2024-12-22 02:02:36 -0300

    Update field types and add CSV tags for improved data handling

--- src/model/vaccine.go
+++ src/model/vaccine.go
@@ -4,15 +4,15 @@ import "time"
 
 // Represents a Vaccine along the time and world regions.
 type Vaccine struct {
-	ID                string    `json:"id"`
-	ISO3              string    `json:"iso3"`
-	Product           string    `json:"product"`
-	Vaccine           string    `json:"vaccine"`
-	Company           string    `json:"company"`
-	AuthorizationDate time.Time `json:"authorization_date"`
-	StartDate         time.Time `json:"start_date"`
-	EndDate           time.Time `json:"end_date"`
-	DataSource        string    `json:"data_source"`
+	ID                int64    `json:"id"`
+	ISO3              string    `json:"iso3" csv:"ISO3"`
+	Product           string    `json:"product" csv:"PRODUCT_NAME"`
+	Vaccine           string    `json:"vaccine" csv:"VACCINE_NAME"`
+	Company           string    `json:"company" csv:"COMPANY_NAME"`
+	AuthorizationDate string    `json:"authorization_date" csv:"AUTHORIZATION_DATE"`
+	StartDate         string    `json:"start_date" csv:"START_DATE"`
+	EndDate           string    `json:"end_date" csv:"END_DATE"`
+	DataSource        string    `json:"data_source" csv:"DATA_SOURCE"`
 	CreatedAt         time.Time `json:"created_at"`
 	UpdatedAt         time.Time `json:"updated_at"`
 }
