0f8913c049042d13c8c6e97f19072203797bb73c
Junior Schmidt <junior.schmidt.99@gmail.com>
2024-12-26 11:29:59 -0300

    Add API routes for COVID-19 data retrieval by country and date

--- src/routes/api.go
+++ src/routes/api.go
@@ -17,5 +17,22 @@ func API(r *gin.Engine) *gin.Engine {
 	{
 		etl.POST("/csv", handler.ProcessCSV)
 	}
+	questions := v1.Group("/questions")
+	{
+		// 1. Total acumulado de casos e mortes de Covid-19 em um país específico em uma data determinada
+		questions.GET("/1", handler.GetTotalCasesAndDeathsByCountryAndDate)
+
+		// 2. Pessoas vacinadas com pelo menos uma dose em um país específico em uma data específica
+		questions.GET("/2", handler.GetVaccinatedPeopleByCountryAndDate)
+
+		// // 3. Vacinas usadas em um país e as datas de início de aplicação
+		// questions.GET("/3", handler.GetVaccinesByCountryAndStartDate)
+
+		// // 4. País com o maior número de casos acumulados até uma data específica
+		// questions.GET("/4", handler.GetCountryWithHighestCasesByDate)
+
+		// // 5. Vacina mais utilizada em uma região específica
+		// questions.GET("/5", handler.GetMostUsedVaccineByRegion)
+	}
 	return r
 }
