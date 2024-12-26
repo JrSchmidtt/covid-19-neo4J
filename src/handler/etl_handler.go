ff96bfb333e526beab05aa990f9e82974b250681
Junior Schmidt <junior.schmidt.99@gmail.com>
2024-12-25 23:56:53 -0300

    [WIP] Extract, Transform, Load Handler

--- src/handler/etl_handler.go
+++ src/handler/etl_handler.go
@@ -1,73 +1,67 @@
 package handler
 
 import (
-	"encoding/csv"
-	"fmt"
+	"mime/multipart"
+	"net/http"
 
 	"github.com/JrSchmidtt/covid-19-neo4J/src/model"
+	"github.com/JrSchmidtt/covid-19-neo4J/src/service"
 	"github.com/gin-gonic/gin"
-	"github.com/gocarina/gocsv"
 )
 
-type fileConfig struct {
-	Key   string
-	Comma rune
-	Model interface{}
-}
 
 func ProcessCSV(c *gin.Context) {
-	filesConfig := []fileConfig{
+	form, err := c.MultipartForm()
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"message": "failed to parse form data",
+			"error":   err.Error(),
+		})
+		return
+	}
+
+	files := make(map[string]*multipart.FileHeader)
+	for key, fileHeaders := range form.File {
+		if len(fileHeaders) > 0 {
+			files[key] = fileHeaders[0]
+		}
+	}
+
+	etlService := service.NewETLService()
+	etlService.SetFileConfig([]service.FileConfig{
 		{Key: "vaccination", Comma: ';', Model: &[]model.Vaccination{}},
 		{Key: "vaccine", Comma: ';', Model: &[]model.Vaccine{}},
 		{Key: "covid_global", Comma: ',', Model: &[]model.CovidGlobal{}},
 		{Key: "covid", Comma: ';', Model: &[]model.Covid{}},
-	}
-
-	var errors []string
-	var warnings []string
-	parsedData := make(map[string]interface{})
-
-	for _, fileCfg := range filesConfig {
-		fileData, err := processFile(c, fileCfg)
-		if err != nil {
-			errors = append(errors, fmt.Sprintf("Error processing file '%s': %v", fileCfg.Key, err))
-			continue
-		}
-
-		parsedData[fileCfg.Key] = fileData
-	}
-
-	if len(errors) > 0 {
-		c.JSON(422, gin.H{
-			"message":  "error processing some files",
+	})
+	
+	parsedData, errors, warnings, err := etlService.Extract(files)
+	if err != nil {
+		c.JSON(http.StatusUnprocessableEntity, gin.H{
+			"message":  "error processing files",
 			"errors":   errors,
 			"warnings": warnings,
 			"data":     parsedData,
 		})
 		return
 	}
+	if err := etlService.Transform(parsedData); err != nil {
+		c.JSON(500, gin.H{
+			"message": "error transforming data",
+			"error":   err.Error(),
+		})
+		return
+	}
+	// vaccination := parsedData["vaccination"].(*[]model.Vaccination)
+	// vaccine := parsedData["vaccine"].(*[]model.Vaccine)
+	// covidGlobal := parsedData["covid_global"].(*[]model.CovidGlobal)
+	// covid := parsedData["covid"].(*[]model.Covid)
 
-	c.JSON(200, gin.H{
+	c.JSON(http.StatusOK, gin.H{
 		"message":  "success",
 		"errors":   errors,
 		"warnings": warnings,
+		 "data":     parsedData,
 		// "data":     parsedData, // TODO: Make this a query parameter.
 	})
-}
-
-func processFile(c *gin.Context, cfg fileConfig) (interface{}, error) {
-	file, _, err := c.Request.FormFile(cfg.Key)
-	if err != nil {
-		return nil, fmt.Errorf("failed to retrieve file: %w", err)
-	}
-	defer file.Close()
-
-	csvReader := csv.NewReader(file)
-	csvReader.Comma = cfg.Comma
-
-	if err := gocsv.UnmarshalCSV(csvReader, cfg.Model); err != nil {
-		return nil, fmt.Errorf("error unmarshalling CSV data: %w", err)
-	}
-
-	return cfg.Model, nil
 }
