8c8c4b8e9ee3942fff326ec9050d6c75234ee23a
Junior Schmidt <junior.schmidt.99@gmail.com>
2024-12-26 13:09:26 -0300

    Add GetVaccinesByCountryAndStartDate

--- src/handler/questions.go
+++ src/handler/questions.go
@@ -1,6 +1,7 @@
 package handler
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/JrSchmidtt/covid-19-neo4J/src/service"
@@ -25,6 +26,35 @@ func GetVaccinatedPeopleByCountryAndDate(c *gin.Context) {
 	country_code := c.Query("country_code")
 	date := c.Query("date")
 	result, err := service.GetVaccinatedPeopleByCountryAndDate(country_code, date)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{
+		"message": "success",
+		"data": result,
+	})
+}
+
+func GetVaccinesByCountryAndStartDate(c *gin.Context) {
+	country_code := c.Query("country_code")
+	start_date := c.Query("start_date")
+	fmt.Println("country_code", country_code)
+	fmt.Println("start_date", start_date)
+	result, err := service.GetVaccinesByCountryAndStartDate(country_code, start_date)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{
+		"message": "success",
+		"data": result,
+	})
+}
+
+func GetMostUsedVaccineByRegion(c *gin.Context) {
+	region := c.Query("region")
+	result, err := service.GetMostUsedVaccineByRegion(region)
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 		return
